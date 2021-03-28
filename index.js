c31d6daa55dadf27ce9b775fec09f273074370b6
Nattapong Cheewalertsakul <>
2021-03-28 12:33:13 +0700

    get list image, refresh list

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+const host = 'https://bifrost-boss.ask-dom.com'
+const bucketName = 'new-bucket-from-rest'
 var app = new Vue({
     el: '#app',
     data: {
         file: null,
-        dropFiles: [],
+        // dropFiles: [],
         preview: null,
         isImageLarge: false,
         isImageUploaded: false,
         isUploadImageError: false,
         isLoading: false,
+        images: [],
+        expiredTimeUrl: 60,//second
+    },
+    async mounted() {
+        this.getImages()
     },
     methods: {
+        countDowntime() {
+            if (this.expiredTimeUrl > 0) {
+                setTimeout(() => {
+                    this.expiredTimeUrl -= 1
+                    this.countDowntime()
+                }, 1000)
+            }else{
+                // refresh url
+                this.expiredTimeUrl = 60
+                this.getImages()
+            }
+        },
         previewUpload() {
             this.isImageLarge = false
             this.isImageUploaded = false
@@ -33,9 +52,10 @@ var app = new Vue({
         async uploadImage() {
             this.isLoading = true
             if (this.file) {
-                let uploadSignedURL = await axios.post('http://localhost:3000/signed-url/upload', {
-                    filePath: this.file.name,
-                    fileType: this.file.type
+                let uploadSignedURL = await axios.post(`${host}/bucket/${bucketName}/signed-url/upload`, {
+                    fileName: this.file.name,
+                    fileType: this.file.type,
+                    bucketName: bucketName
                 }).catch(error => {
                     console.log(error)
                     this.isUploadImageError = true
@@ -61,11 +81,31 @@ var app = new Vue({
                         this.isImageUploaded = true
                         this.file = null
                         this.preview = null
+                        this.getImages()
                     }
 
                 }
             }
             this.isLoading = false
+        },
+        async getImages() {
+            this.images = []
+            let imagesList = await axios.get(`${host}/bucket/${bucketName}/images`).catch(error => {
+                console.log(error)
+            })
+            if (imagesList && 'data' in imagesList.data) {
+                for (let i = 0; i < imagesList.data.data.length; i++) {
+                    let m = imagesList.data.data[i]
+                    let updatedDate = new Date(m.updated)
+                    this.images.push({
+                        name: m.name,
+                        size: (m.size / 1000).toFixed(2),
+                        updated: `${updatedDate.toLocaleDateString()} ${updatedDate.toLocaleTimeString()}`,
+                        url: m.url
+                    })
+                }
+            }
+            this.countDowntime()
         }
     }
 
